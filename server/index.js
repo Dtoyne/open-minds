0675f6aed972bc39091ef27c6bc595cbc8e0fcfe
Dtoyne <dylantoyne@gmail.com>
2017-05-02 08:58:54 -0400

    Added main.html

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
+// require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.get('/', function(req, res) {
-  res.send('Open Minds');
+// app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('/main');
 });
+
+// app.post('/', (req, res) => {
+//   if (!req.body) {
+//     res.status(400).json({ error: 'Invalid Request: No input provided'});
+//   } else {
+//     console.log('Submission Successful');
+//   }
+// });
+
+// app.use((req, res) => res.status(404).send(`Error 404. This path does not exist.`));
 
 app.listen(8080, () => console.log(`Listening on port ${PORT}`));
