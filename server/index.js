585739414f848ef5b9e33245f64b173c3a5e88e7
Dtoyne <dylantoyne@gmail.com>
2017-05-04 15:51:11 -0400

    Updated some routes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,48 @@
-// require('dotenv').config();
+require('dotenv').config();
 
 const express       = require('express');
 const app           = express();
 const bodyParser    = require('body-parser');
 const PORT          = 8080;
 
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('/main');
 });
 
-// app.post('/', (req, res) => {
-//   if (!req.body) {
-//     res.status(400).json({ error: 'Invalid Request: No input provided'});
-//   } else {
-//     console.log('Submission Successful');
-//   }
-// });
+app.get('/speakers', (req, res) => {
+  res.render('/speakers');
+});
 
-// app.use((req, res) => res.status(404).send(`Error 404. This path does not exist.`));
+app.get('/about', (req, res) => {
+  res.render('/about');
+});
+
+app.get('/speakerseries', (req, res) => {
+  res.render('/speakerseries');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('/contact');
+});
+
+app.get('/workshops', (req, res) => {
+  res.render('/workshops');
+});
+
+app.get('/archive', (req, res) => {
+  res.render('/archive');
+});
+
+app.post('/', (req, res) => {
+  if (!req.body) {
+    res.status(400).json({ error: 'Invalid Request: No input provided'});
+  } else {
+    console.log('Submission Successful');
+  }
+});
+
+app.use((req, res) => res.status(404).send(`Error 404. This path does not exist.`));
 
 app.listen(8080, () => console.log(`Listening on port ${PORT}`));
